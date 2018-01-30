86287d8a5b2880e27b416dd540b04a1ace24e899
Baptiste CHOCOT <baptiste.chocot@telecomnancy.net>
2018-01-30 10:10:54 +0100

    made it works!

--- src/main/java/com/moresnowballs/moresnowballsmod/entity/EntityFlintSnowball.java
+++ src/main/java/com/moresnowballs/moresnowballsmod/entity/EntityFlintSnowball.java
@@ -1,9 +1,7 @@
 package com.moresnowballs.moresnowballsmod.entity;
 
-import com.moresnowballs.moresnowballsmod.MoreSnowballsMod;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.projectile.EntitySnowball;
 import net.minecraft.entity.projectile.EntityThrowable;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.EnumParticleTypes;
@@ -13,7 +11,7 @@ import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class EntityFlintSnowball extends EntitySnowball {
+public class EntityFlintSnowball extends EntityThrowable {
     public EntityFlintSnowball(World worldIn) { super(worldIn); }
 
     public EntityFlintSnowball(World worldIn, EntityLivingBase throwerIn)
@@ -33,18 +31,16 @@ public class EntityFlintSnowball extends EntitySnowball {
 
     /**
      *
-import net.minecraftforge.fml.common.registry.EntityRegistry;Handler for {@link World#setEntityState}
+import net.minecraftforge.fml.common.registry.EntityRegistry.Handler for {@link World#setEntityState}
      */
     @SideOnly(Side.CLIENT)
     public void handleStatusUpdate(byte id)
     {
-        MoreSnowballsMod.LOGGER.info("handleStatusUpdate(id) : id = " + id);
         if (id == 3)
         {
             for (int i = 0; i < 8; ++i)
             {
-                //this.world.spawnParticle(EnumParticleTypes.SNOWBALL, this.posX, this.posY, this.posZ, 0.0D, 0.0D, 0.0D);
-                this.world.spawnParticle(EnumParticleTypes.SPELL, this.posX, this.posY, this.posZ, 0.0D, 0.0D, 0.0D);
+                this.world.spawnParticle(EnumParticleTypes.EXPLOSION_NORMAL, this.posX, this.posY, this.posZ, 0.0D, 0.0D, 0.0D);
             }
         }
     }
@@ -52,6 +48,7 @@ import net.minecraftforge.fml.common.registry.EntityRegistry;Handler for {@link 
     /**
      * Called when this EntityThrowable hits a block or entity.
      */
+    @Override
     protected void onImpact(RayTraceResult result)
     {
         if (result.entityHit != null)
