f48fa38b8a301d2448015f166fa2328396484d2c
Baptiste CHOCOT <baptiste.chocot@telecomnancy.net>
2018-01-30 10:16:58 +0100

    added all existing snowballs

--- src/main/java/com/moresnowballs/moresnowballsmod/proxy/ServerProxy.java
+++ src/main/java/com/moresnowballs/moresnowballsmod/proxy/ServerProxy.java
@@ -1,7 +1,7 @@
 package com.moresnowballs.moresnowballsmod.proxy;
 
 import com.moresnowballs.moresnowballsmod.MoreSnowballsMod;
-import com.moresnowballs.moresnowballsmod.entity.EntityFlintSnowball;
+import com.moresnowballs.moresnowballsmod.entity.*;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.registry.EntityRegistry;
@@ -12,5 +12,9 @@ public class ServerProxy extends CommonProxy {
     public void preInit(FMLPreInitializationEvent event){
         super.preInit(event);
         EntityRegistry.registerModEntity(new ResourceLocation(MoreSnowballsMod.MODID, "moresnowballs"), EntityFlintSnowball.class, MoreSnowballsMod.MODID + ".flint_snowball", 38, MoreSnowballsMod.instance, 64, 3, true);
+        EntityRegistry.registerModEntity(new ResourceLocation(MoreSnowballsMod.MODID, "moresnowballs"), EntityFireSnowball.class, MoreSnowballsMod.MODID + ".fire_snowball", 39, MoreSnowballsMod.instance, 64, 3, true);
+        EntityRegistry.registerModEntity(new ResourceLocation(MoreSnowballsMod.MODID, "moresnowballs"), EntityPoisonSnowball.class, MoreSnowballsMod.MODID + ".poison_snowball", 40, MoreSnowballsMod.instance, 64, 3, true);
+        EntityRegistry.registerModEntity(new ResourceLocation(MoreSnowballsMod.MODID, "moresnowballs"), EntityTNTSnowball.class, MoreSnowballsMod.MODID + ".tnt_snowball", 41, MoreSnowballsMod.instance, 64, 3, true);
+        EntityRegistry.registerModEntity(new ResourceLocation(MoreSnowballsMod.MODID, "moresnowballs"), EntityWitherSnowball.class, MoreSnowballsMod.MODID + ".wither_snowball", 42, MoreSnowballsMod.instance, 64, 3, true);
     }
 }
